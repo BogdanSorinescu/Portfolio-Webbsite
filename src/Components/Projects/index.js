299b9c017ffff2abe035ae52b2309d3a9fa62439
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-08-24 16:10:36 +0200

    Finished layout of Project page

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,10 +1,59 @@
 
+import AnimatedLetters from "../AnimatedLetters";
+import { useEffect, useState } from "react";
 import "./index.scss";
+import Loader from "react-loaders";
 
 const Projects = () => {
-  return <div className="projects"></div>;
+  const [loading, setLoading] = useState(true);
+  const [letterClass, setLetterClass] = useState('text-animate');
 
-  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (!loading) {
+      const timer = setTimeout(() => {
+        setLetterClass("text-animate-hover");
+      }, 3000);
+      return () => clearTimeout(timer);
+    }
+  }, [loading]);
+
+   if (loading){
+        return <div className='loader'></div>
+    }
+
+  return (
+    <div className="projects">
+      <h1 className="page-title">
+        <AnimatedLetters
+          letterClass={letterClass}
+          strArray={["P", "r", "o", "j", "e", "c", "t", "s"]}
+          idx={15}
+        />
+      </h1>
+
+      <div className="projects-container">
+        <div className="slide-cards">
+          <h2>Porjects</h2>
+          <p>Responsive Portfolio website made with JavaScript, React and SCSS.</p>
+        </div>
+
+        <div className="slide-cards">
+          <h2>Self service kiosk</h2>
+          <p>Univeersity project made with PostgreSQL backend, Java for functionality and JavaFX.</p>
+        </div>
+      </div>
+    </div>
+  )
+
+
+
 };
 
 export default Projects;
