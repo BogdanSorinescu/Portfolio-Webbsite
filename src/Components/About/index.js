35642e438155a942b73d2845d55aaf3d86c870ad
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-08-30 12:35:51 +0200

    Updated the about me text

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -40,15 +40,16 @@ const About = () => {
                             idx={15}
                         />
                     </h1>
-                    <p>I am an ambitious, hardworking, and cooperative individual who aspires to gain extensive experience in the IT field.
-                        I am seeking a position at an established IT company that offers the opportunity to learn the latest technologies and methodologies in software development.
-                        I am eager to contribute my skills while continuously growing and developing professionally.</p>
+                    <p>Hello, I'm Bogdan, I am currently starting my second year of my computer science/software technology studies in Sweden, Linnaeus University. I am a passionate learner with a indomitable ambition especially when it comes to results and learning new things and to become the best version of myself. </p>
                     <p>
                         I am a confident, curious, creative, and ambitious individual who loves learning from others
-                        as well as working collaboratively in a team.
+                        as well as working collaboratively in a team, that also being the best way I can learn new technologies and methodologies.
                     </p>
+
+                    <p>So far ive had some experience with the following: Java, Python, JavaScript, Html5, css, scss, react framework, entity framework, C#, PostgreSQL and of course SQL and with plans to expand my skills for the future of course.</p>
+                    <p>For the moment since Ive only recently finished my first year in compute science ive contributed to a project based in Java, utilizing PostgreSQL and docker for the backend and JavaFX for the overall UI and front-end. Thus being my first fullstack project where i also was the project manager for our small group of 4 people.</p>
                     <p>I would describe myself as an active, social, and extroverted person
-                        who loves adventures, traveling, and all things tech. I am also a big sports fan with a passion for working out.</p>
+                        who loves adventures, traveling, and all things tech, especially learning new programming languages. I am also a big sports fan with a passion for working out. As well as a huge believer in that nothing is impossible, and always push my limits no matter the circumstances in order to reach my goals/objectives wether thats in my personal or work life.</p>
                 </div>
 
                 <div className='stage-cube-cont'>
