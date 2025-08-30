eede5b7a59812aa131308695788dfd3d61f12f47
Bogdan Sorinescu <bogdansorinescu@gmail.com>
2025-08-30 14:02:52 +0200

    addes skill bars in about page

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,15 +1,23 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { faAngular, faCss, faDocker, faGit, faHtml5, faJava, faPython, faReact } from '@fortawesome/free-brands-svg-icons';
 import Loader from 'react-loaders';
 
 const About = () => {
-
     const [letterClass, setLetterClass] = useState('text-animate');
     const [loading, setLoading] = useState(true);
+    const [loadedBars, setLoadedBars] = useState(false);
+
+    const skillsData = [
+        { name: 'Python (1 year)', value: 78, color: '#306998' },
+        { name: 'Java (9 months)', value: 68, color: '#dd6300' },
+        { name: 'PostgreSQL (4 months)', value: 65, color: '#ef5b65' },
+        { name: 'CSS (3 months)', value: 45, color: '#28A4D9' },
+        { name: 'JavaScript(3 months)', value: 30, color: '#f7df1e' },
+        { name: 'C# (3 months)', value: 50, color: '#68217A' }
+    ];
 
     useEffect(() => {
         const timer = setTimeout(() => {
@@ -18,6 +26,7 @@ const About = () => {
 
         const timer2 = setTimeout(() => {
             setLoading(false);
+            setLoadedBars(true);
         }, 1800);
 
         return () => {
@@ -26,66 +35,65 @@ const About = () => {
         };
     }, []);
 
-    if (loading){
-        return <div className='loader'></div>
-    }
+    if (loading) return <div className='loader'></div>;
+
     return (
-        <>
-            <div className='container about-page'>
-                <div className='text-zone'>
-                    <h1>
-                        <AnimatedLetters
-                            letterClass={letterClass}
-                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-                            idx={15}
-                        />
-                    </h1>
-                    <p>Hello, I'm Bogdan, I am currently starting my second year of my computer science/software technology studies in Sweden, Linnaeus University. I am a passionate learner with a indomitable ambition especially when it comes to results and learning new things and to become the best version of myself. </p>
-                    <p>
-                        I am a confident, curious, creative, and ambitious individual who loves learning from others
-                        as well as working collaboratively in a team, that also being the best way I can learn new technologies and methodologies.
-                    </p>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+                        idx={15}
+                    />
+                </h1>
+                <p>Hello, I'm Bogdan. I am currently starting my second year of my computer science/software technology studies in Sweden, Linnaeus University. I am a passionate learner with an indomitable ambition, especially when it comes to results, learning new things, and becoming the best version of myself.</p>
 
-                    <p>So far ive had some experience with the following: Java, Python, JavaScript, Html5, css, scss, react framework, entity framework, C#, PostgreSQL and of course SQL and with plans to expand my skills for the future of course.</p>
-                    <p>For the moment since Ive only recently finished my first year in compute science ive contributed to a project based in Java, utilizing PostgreSQL and docker for the backend and JavaFX for the overall UI and front-end. Thus being my first fullstack project where i also was the project manager for our small group of 4 people.</p>
-                    <p>I would describe myself as an active, social, and extroverted person
-                        who loves adventures, traveling, and all things tech, especially learning new programming languages. I am also a big sports fan with a passion for working out. As well as a huge believer in that nothing is impossible, and always push my limits no matter the circumstances in order to reach my goals/objectives wether thats in my personal or work life.</p>
-                </div>
+                <p>I am a confident, curious, creative, and ambitious individual who loves learning from others as well as working collaboratively in a team, which is also the best way I can learn new technologies and methodologies.</p>
 
-                <div className='stage-cube-cont'>
-                    <div className='cubespinner'>
-                        <div className='face1'>
-                            <FontAwesomeIcon icon={faPython} color="#008cddff" />
-                        </div>
+                <p>So far, I've had some experience with the following: Java, Python, JavaScript, HTML5, CSS, SCSS, React framework, Entity Framework, C#, PostgreSQL, and of course SQL, with plans to expand my skills further in the future.</p>
 
-                        <div className='face2'>
-                            <FontAwesomeIcon icon={faHtml5} color="#ff0202ff" />
-                        </div>
+                <p>For the moment, since I've only recently finished my first year in computer science, I've contributed to a project based in Java, utilizing PostgreSQL and Docker for the backend and JavaFX for the overall UI and front-end. This was my first full-stack project, where I also acted as the project manager for our small group of 4 people.</p>
 
-                        <div className='face3'>
-                            <FontAwesomeIcon icon={faCss} color="#28A4D9" />
-                        </div>
+                <p>I would describe myself as an active, social, and extroverted person who loves adventures, traveling, and all things tech, especially learning new programming languages. I am also a big sports fan with a passion for working out. I am a strong believer that nothing is impossible and always push my limits no matter the circumstances to reach my goals/objectives, whether in my personal or work life.</p>
 
-                        <div className='face4'>
-                            <FontAwesomeIcon icon={faDocker} color="#1085f3ff" />
-                        </div>
-                        <div className='face5'>
-                            <FontAwesomeIcon icon={faJava} color="#dd6300ff" />
-                        </div>
 
-                        <div className='face6'>
-                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                <div className='skills'>
+                    {skillsData.map((skill, idx) => (
+                        <div className='skill' key={idx} style={{ marginBottom: '20px' }}>
+                            <span className='skill-name'>{skill.name}</span>
+                            <div style={{
+                                width: '400px',
+                                backgroundColor: 'rgba(255, 255, 255, 0.1)',
+                                borderRadius: '4px',
+                                overflow: 'hidden'
+                            }}>
+                                <div style={{
+                                    width: loadedBars ? `${skill.value}%` : '0%',
+                                    backgroundColor: skill.color,
+                                    height: '20px',
+                                    borderRadius: '4px',
+                                    transition: 'width 1.5s ease-in-out'
+                                }}></div>
+                            </div>
                         </div>
-                        <div className='face7'>
-                            <FontAwesomeIcon icon={faGit} color="#fc7147ff" />
-                        </div>
-                    </div>
-
+                    ))}
                 </div>
             </div>
 
-        </>
-    )
+            <div className='stage-cube-cont'>
+                <div className='cubespinner'>
+                    <div className='face1'><FontAwesomeIcon icon={faPython} color="#008cddff" /></div>
+                    <div className='face2'><FontAwesomeIcon icon={faHtml5} color="#ff0202ff" /></div>
+                    <div className='face3'><FontAwesomeIcon icon={faCss} color="#28A4D9" /></div>
+                    <div className='face4'><FontAwesomeIcon icon={faDocker} color="#1085f3ff" /></div>
+                    <div className='face5'><FontAwesomeIcon icon={faJava} color="#dd6300ff" /></div>
+                    <div className='face6'><FontAwesomeIcon icon={faReact} color="#5ED4F4" /></div>
+                    <div className='face7'><FontAwesomeIcon icon={faGit} color="#fc7147ff" /></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default About; 
+export default About;
